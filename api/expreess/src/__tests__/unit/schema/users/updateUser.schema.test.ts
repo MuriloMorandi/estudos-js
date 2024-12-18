55d57944f13dcee829a0c395aa098cada58a6e03
Murilo Morandi Alexandre <murilomorandialexandre2011@gmail.com>
2024-12-18 07:35:19 -0300

    :test_tube: test(api): Corrigindo os teste de schema de atualizacao de usuarios

--- api/expreess/src/__tests__/unit/schema/users/updateUser.schema.test.ts
+++ api/expreess/src/__tests__/unit/schema/users/updateUser.schema.test.ts
@@ -1,39 +1,39 @@
 import updateUserSchema from "@schemas/user/updateUser.schema";
+import { nanoid } from "nanoid";
 
+const idTest = nanoid();
 
 describe("Validação do updateUserSchema", () => {
-  
 
   it("Deve validar um input válido", () => {
     const validInput = {
-        id: 1,
-        name: "John",
-        email: "john@example.com",
+      id: idTest,
+      name: "John",
+      email: "john@example.com",
     };
       
     const result = updateUserSchema.safeParse(validInput);
     expect(result.success).toBe(true);
   });
-
   
-    it("Deve falhar se id for menor que 1", () => {
-        const invalidInput = {
-            id: 0,
-            name: "John",
-            email: "john@example.com",
-        };
-    
-        const result = updateUserSchema.safeParse(invalidInput);
-        expect(result.success).toBe(false);
-        expect(result.error?.errors[0].message).toBe("ID inválido");
-    });
+  it("Deve falhar se id for menor que 1", () => {
+    const invalidInput = {
+      id: 'asd',
+      name: "John",
+      email: "john@example.com",
+    };
+
+    const result = updateUserSchema.safeParse(invalidInput);
+    expect(result.success).toBe(false);
+    expect(result.error?.errors[0].message).toBe("ID inválido");
+  });
 
     it("Deve falhar se id não for um número inteiro", () => {
      const invalidInput = {
-            id: 1.5,
-            name: "John",
-            email: "john@example.com",
-        };
+        id: idTest,
+        name: "John",
+        email: "john@example.com",
+      };
 
       const result = updateUserSchema.safeParse(invalidInput);
       expect(result.success).toBe(false);
@@ -51,61 +51,60 @@ describe("Validação do updateUserSchema", () => {
   
 
   
-    it("Deve falhar se name tiver menos de 2 caracteres", () => {
-        const invalidInput = {
-          id:1,
-            name: "j",
-            email: "john@example.com",
-        };
-
-      const result = updateUserSchema.safeParse(invalidInput);
-      expect(result.success).toBe(false);
-      expect(result.error?.errors[0].message).toBe("Mínimo 2 caracteres");
-    });
-
-    it("Deve falhar se name for apenas espaços", () => {
-     const invalidInput = {
-         id: 1,   
-         name: "  ",
+  it("Deve falhar se name tiver menos de 2 caracteres", () => {
+      const invalidInput = {
+        id: idTest,
+        name: "j",
         email: "john@example.com",
-        };
+      };
+
+    const result = updateUserSchema.safeParse(invalidInput);
+    expect(result.success).toBe(false);
+    expect(result.error?.errors[0].message).toBe("Mínimo 2 caracteres");
+  });
+
+  it("Deve falhar se name for apenas espaços", () => {
+    const invalidInput = {
+      id: idTest,   
+      name: "  ",
+      email: "john@example.com",
+    };
+    const result = updateUserSchema.safeParse(invalidInput);
+    expect(result.success).toBe(false);
+    expect(result.error?.errors[0].message).toBe("Mínimo 2 caracteres");
+  });
+
+  it("Deve falhar se name for ausente", () => {
+      const invalidInput = {
+        id: idTest,
+        email: "john@example.com",
+      };
+
       const result = updateUserSchema.safeParse(invalidInput);
       expect(result.success).toBe(false);
-      expect(result.error?.errors[0].message).toBe("Mínimo 2 caracteres");
-    });
-
-    it("Deve falhar se name for ausente", () => {
-        const invalidInput = {
-            id: 1,
-            email: "john@example.com",
-        };
-
-        const result = updateUserSchema.safeParse(invalidInput);
-        expect(result.success).toBe(false);
-    });
+  });
   
 
-  describe("Campo email", () => {
-      it("Deve falhar se email não for um formato válido", () => {
-        const invalidInput = {
-            id: 1,
-            name: "John",
-            email: "invalid-email",
-        };
-        
-        const result = updateUserSchema.safeParse(invalidInput);
-        expect(result.success).toBe(false);
-        expect(result.error?.errors[0].message).toBe("E-mail inválido");
-    });
-
-      it("Deve falhar se email for ausente", () => {
-        const invalidInput = {
-            id: 1,
-            name: "John",
-        };
-          
-        const result = updateUserSchema.safeParse(invalidInput);
-        expect(result.success).toBe(false);
-    });
+  it("Deve falhar se email não for um formato válido", () => {
+    const invalidInput = {
+      id: idTest,
+      name: "John",
+      email: "invalid-email",
+    };
+    
+    const result = updateUserSchema.safeParse(invalidInput);
+    expect(result.success).toBe(false);
+    expect(result.error?.errors[0].message).toBe("E-mail inválido");
   });
+
+  it("Deve falhar se email for ausente", () => {
+    const invalidInput = {
+      id: idTest,
+      name: "John",
+    };
+      
+    const result = updateUserSchema.safeParse(invalidInput);
+    expect(result.success).toBe(false);
+  });
+  
 });
